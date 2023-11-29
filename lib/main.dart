25488f60e45357f183748613ba56e7c7d16693f1
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2023-11-29 03:56:26 -0600

    version nueva

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,21 @@ import 'package:myapp/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:myapp/providers/movies_provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider()),
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,14 +26,11 @@ class MyApp extends StatelessWidget {
       title: 'Peliculas',
       initialRoute: 'home',
       routes: {
-        'home':( _ ) =>  HomeScreen(),
-        'details':( _ ) => DetailsScreen(),
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
       },
-      theme: ThemeData.light().copyWith(
-        appBarTheme: AppBarTheme(
-          color: Colors.orange
-        )
-      ),
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: AppBarTheme(color: Colors.orange)),
     );
   }
 }
