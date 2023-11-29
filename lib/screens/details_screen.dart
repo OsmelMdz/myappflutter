7ff5e84cd294c141f781e06471d346bd5def5f3d
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2023-11-29 04:09:50 -0600

    casting cards

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
   /* const DetailsScreen({super.key}); */
@@ -23,6 +24,15 @@ class DetailsScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+              _Overview(),
+              _Overview(),
+              _Overview(),
+              CastingCards(),
+            ]),
+          ),
         ],
       ),
     );
@@ -54,6 +64,81 @@ class _CustomAppBar extends StatelessWidget {
           image: AssetImage('assets/loading.gif'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Hero(
+            tag: 'movie.hero',
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: AssetImage('assets/no-image.jpg'),
+                height: 150,
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 150),
+                child: Text(
+                  'movie.title',
+                  style: textTheme.headline5,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 150),
+                child: Text(
+                  'movie.originalTitle',
+                  style: textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  SizedBox(width: 5),
+                  Text(
+                    'movie.voteAverage',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Text(
+        'movie.overview',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
       ),
     );
   }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:myapp/widgets/card_swiper.dart';
 export 'package:myapp/widgets/movie_slider.dart';
+export 'package:myapp/widgets/casting_cards.dart';
